9075f10302b82141cfaa34641e0779ac5e050f2a
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-05-16 11:28:36 +0100

    Start work on CLJXifying the project
    Update the project.clj file to use CLJX.

--- project.clj
+++ project.clj
@@ -3,4 +3,21 @@
   :url "https://github.com/listora/elect"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]
+                   :plugins [[com.keminglabs/cljx "0.3.2"]]
+                   :hooks [cljx.hooks]
+                   :cljx {:builds [{:source-paths ["src/cljx"]
+                                    :output-path "target/classes"
+                                    :rules :clj}
+                                   {:source-paths ["src/cljx"]
+                                    :output-path "target/classes"
+                                    :rules :cljs}
+                                   {:source-paths ["test/cljx"]
+                                    :output-path "target/test-classes"
+                                    :rules :clj}
+                                   {:source-paths ["test/cljx"]
+                                    :output-path "target/test-classes"
+                                    :rules :cljs}]}}}
+
+  :jar-exclusions [#"\.cljx$"])
